0e4d4ff68eee91f46ee6daaffa94abfca7a78254
zhoushangren <zhoushangren@yahoo.com>
2016-09-30 08:59:10 +0800

    添加了一些JS

---
+++ Page/ss.zhoushangren.com/help/main.js
@@ -0,0 +1,20 @@
+function loading_img(){
+    var i=document.getElementsByTagName('img');
+    if(i.length===0){
+        return false;
+    }
+    var z=i;
+    for(var y=0;y<z.length;y++){
+        z[y].src='loading.gif';
+    }
+    var img=new Array();
+    for(var x=0;x<i.length;x++){
+        img[x]=new Image();
+        img[x].src=i[x].src;
+    }
+    window.onload=function(){
+        for(var x=0;x<i.length;x++){
+            i[x].src=i[x].src;
+        }
+    }
+}
