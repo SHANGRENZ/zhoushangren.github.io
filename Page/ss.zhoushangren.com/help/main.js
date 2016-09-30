f07004cb0cbe7b9f37e48ba55d8bedb05a212a78
zhoushangren <zhoushangren@yahoo.com>
2016-09-30 09:17:42 +0800

    更新了JS文件

--- Page/ss.zhoushangren.com/help/main.js
+++ Page/ss.zhoushangren.com/help/main.js
@@ -1,18 +1,17 @@
 var i=document.getElementsByTagName('img');
-if(i.length===0){
-    return;
-}
-var z=i;
-for(var y=0;y<z.length;y++){
-    z[y].src='loading.gif';
-}
-var img=new Array();
-for(var x=0;x<i.length;x++){
-    img[x]=new Image();
-    img[x].src=i[x].src;
-}
-window.onload=function(){
+if(i.length!==0){
+    var z=i;
+    for(var y=0;y<z.length;y++){
+        z[y].src='loading.gif';
+    }
+    var img=new Array();
     for(var x=0;x<i.length;x++){
-        i[x].src=i[x].src;
+        img[x]=new Image();
+        img[x].src=i[x].src;
+    }
+    window.onload=function(){
+        for(var x=0;x<i.length;x++){
+            i[x].src=i[x].src;
+        }
     }
 }
