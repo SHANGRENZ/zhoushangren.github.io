388bdd588f3a696407e6671794fcf2c58bdaace0
zhoushangren <zhoushangren@yahoo.com>
2016-10-30 22:10:36 +0800

    2016-10-30

---
+++ Page/XueBaJiHua/English/english_data.js
@@ -0,0 +1,292 @@
+var english_data={
+    "title":[
+        "Unit 1.My name's Gina.",
+        "Unit 2.This is my sister.",
+        "Unit 3.Is this your pencil?",
+        "Unit 4.Where's my schoolbag?",
+        "Unit 5.Do you have a soccer ball?"
+    ],
+    "data":{
+        "grammar":[
+            [
+                [
+                    "What's your name?",
+                    "Alan./I'm Alan./My name's Alan."
+                ],
+                [
+                    "What's his name?",
+                    "He's Eric./His name's Eric."
+                ],
+                [
+                    "What's hre name?",
+                    "She's Mary./Her name's Mary."
+                ],
+                [
+                    "Is he Jack?",
+                    "Yes,he is./No,he isn't.His name's Mike."
+                ],
+                [
+                    "Are you Helen?",
+                    "Yes,I am./No,I'm not.I'm Gina."
+                ]
+            ],
+            [
+                [
+                    "This is my firend Jane.",
+                    "That's my grandfather."
+                ],
+                [
+                    "These are my brothers.",
+                    "Those are my parents."
+                ],
+                [
+                    "Who's she?",
+                    "She's my sister."
+                ],
+                [
+                    "Who's he?",
+                    "He's my brother."
+                ],
+                [
+                    "Who're they?",
+                    "They're my grandparents."
+                ]
+            ],
+            [
+                [
+                    "Is this your pencil?",
+                    "Yes,it is.It's mine./No,it isn't.It's hers."
+                ],
+                [
+                    "Is this his green pen?",
+                    "Yes,it is./No,it isn't.The blue pen is his."
+                ],
+                [
+                    "Is that your schoolbag?",
+                    "Yes,it is./No,it isn't.It's his."
+                ],
+                [
+                    "Are these your books?",
+                    "Yes,they are./No,they aren't.They're hers."
+                ],
+                [
+                    "Are those her keys?",
+                    "Yes,they are./No,they aren't.They're mine."
+                ]
+            ],
+            [
+                [
+                    "Where's the map?",
+                    "It's in your grandparents' room."
+                ],
+                [
+                    "Where are my books?",
+                    "They're on the sofa."
+                ],
+                [
+                    "Where's his pencil box?",
+                    "It's in his schoolbag."
+                ],
+                [
+                    "Where's your ruler?",
+                    "It's under the chair."
+                ],
+                [
+                    "Where are their keys?",
+                    "They're on the table."
+                ]
+            ],
+            [
+                [
+                    "Do you have a baseball?",
+                    "Yes,I do./No,I don't.I have a volleyball."
+                ],
+                [
+                    "Do you have a ping-pong bat?",
+                    "Yes,I do./No,I don't.I have a ping-pong ball."
+                ],
+                [
+                    "Does she have a tennis ball?",
+                    "Yes,she does./No,she doesn't.She has a baseball."
+                ],
+                [
+                    "Does he have a soccer ball?",
+                    "Yes,he does./No,he doesn't.He has two ping-pong bats."
+                ],
+                [
+                    "Do they have a basketball?",
+                    "Yes,they do./NO,they don't.They have a volleyball."
+                ]
+            ]
+        ],
+        "grammar_p":[
+            [
+                "what's=what is",
+                "name's=name is",
+                "I'm=I am",
+                "he's=he is",
+                "she's=she is"
+            ],
+            [
+                "that's=that is",
+                "who're=who are",
+                "who's=who is",
+                "they're=they are"
+            ],
+            [
+                "it's=it is",
+                "isn't=is not",
+                "aren't=are not"
+            ],
+            [
+                "Where's=Where is"
+            ],
+            [
+                "don't=do not",
+                "doesn't=does not"
+            ]
+        ],
+        "conversation":[
+            {
+                "more_people":false,
+                "people":[
+                    "Linda",
+                    "Helen"
+                ],
+                "content":[
+                    "Good afternoon!My name's Linda.Are you Helen?",
+                    "Yes,I am.Nice to meet you,Linda.",
+                    "Nice to meet you,too.What's her name?",
+                    "She's Jane.",
+                    "Is he Jack?",
+                    "No,he isn't.His name's Mike."
+                ]
+            },
+            {
+                "more_people":true,
+                "people":[
+                    "Sally",
+                    "Jane",
+                    "Sally",
+                    "Kate",
+                    "Jane",
+                    "Kate",
+                    "Jane",
+                    "Sally",
+                    "Jane",
+                    "Sally/Kate"
+                ],
+                "content":[
+                    "Good morning,Jane.",
+                    "Good morning,Sally.",
+                    "Oh,Jane,this is my sister Kate.Kate,this is my friend Jane.",
+                    "Nice to meet you,Jane.",
+                    "Nice to meet you,too.Are those your parents?",
+                    "Yes,they are.",
+                    "And who's he?",
+                    "He's my brother,Paul.",
+                    "Oh,I see.Well,have a good day!",
+                    "Thanks!You,too.Bye!"
+                ]
+            },
+            {
+                "more_people":false,
+                "people":[
+                    "Teacher",
+                    "Anna"
+                ],
+                "content":[
+                    "Hi,Anna.Are these your pencils?",
+                    "No,they're Bob's.",
+                    "And is this his green pen?",
+                    "No,it isn't.The blue pen is his.",
+                    "What about this dictionary?",
+                    "It's Helen's.And the green pen is hers,too.",
+                    "And the eraser?Is that yours?",
+                    "Yes,it is.",
+                    "Thank you for your help,Anna.",
+                    "You're welcome."
+                ]
+            },
+            {
+                "more_people":false,
+                "people":[
+                    "Mom",
+                    "Jack"
+                ],
+                "content":[
+                    "Come on,Jack!",
+                    "Oh,no!Where's my bag?",
+                    "Hmm...is it on your desk?",
+                    "No.And it's not under the chair.",
+                    "Oh!It's on the sofa.",
+                    "Thank you,Mom.Err...where's the map?",
+                    "I think it's in your grandparents' room.",
+                    "Yes,it's on their bed!And my hat?",
+                    "It's on your head!",
+                    "Oh,yech!Haha!"
+                ]
+            },
+            {
+                "more_people":false,
+                "people":[
+                    "Cindy",
+                    "Helen"
+                ],
+                "content":[
+                    "Hey,Helen,let's go!We're late!",
+                    "OK.",
+                    "Do you have the baseball?",
+                    "Yes,I do.It's in my bag.",
+                    "And where's our baseball bat?",
+                    "Bill has it.",
+                    "Oh,yeah.And do you have your jacket?",
+                    "Oh,no,I don't.It's on the chair.Let me get it.",
+                    "And your hat,too!",
+                    "OK,I have my jacket and hat.Let's go!"
+                ]
+            }
+        ],
+        "media":[
+            "media/unit1.mp3",
+            "media/unit2.mp3",
+            "media/unit3.mp3",
+            "media/unit4.mp3",
+            "media/unit5.mp3"
+        ]
+    }
+}
+function set_data(data){
+    var all_data='<!--使用Javascript填充-->';
+    for(var x=0;x<data.data.grammar.length;x++){
+        all_data+='<h1>'+data.title[x]+'</h1>';
+        all_data+='<h2>Grammar Focus</h2>';
+        all_data+='<table>';
+        for(var i=0;i<data.data.grammar[x].length;i++){
+            all_data+='<tr>';
+            all_data+='<td>'+data.data.grammar[x][i][0]+'</td>';
+            all_data+='<td>'+data.data.grammar[x][i][1]+'</td>';
+            all_data+='</tr>';
+        }
+        all_data+='</table>';
+        all_data+='<p class="grammar_focus">';
+        for(var i=0;i<data.data.grammar_p[x].length;i++){
+            all_data+='<b>'+data.data.grammar_p[x][i]+'</b>';
+        }
+        all_data+='</p>';
+        all_data+='<h2>Role-play the conversation</h2>';
+        if(data.data.conversation[x].more_people){
+            for(var i=0;i<data.data.conversation[x].people.length;i++){
+                all_data+='<p><b>'+data.data.conversation[x].people[i]+':</b>'+data.data.conversation[x].content[i]+'</p>';
+            }
+        }else{
+            for(var i=0;i<data.data.conversation[x].content.length/2;i++){
+                all_data+='<p><b>'+data.data.conversation[x].people[0]+':</b>'+data.data.conversation[x].content[i*2]+'</p>';
+                all_data+='<p><b>'+data.data.conversation[x].people[1]+':</b>'+data.data.conversation[x].content[i*2+1]+'</p>';
+            }
+        }
+        all_data+='<audio src="'+data.data.media[x]+'" controls="controls" id="mp3_'+(x+1)+'">你的浏览器太过陈旧,不支持播放音频.</audio>';
+        all_data+='<input type="button" class="set_audio_loop" value="开启循环" onclick="set_audio_loop(\'mp3_'+(x+1)+'\')" id="mp3_'+(x+1)+'_btn"/>';
+    }
+    document.getElementById('body_div').innerHTML=all_data;
+}
